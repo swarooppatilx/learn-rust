43cb248ba209b1551fc9e72dcd1a932f03b1f0a6
swarooppatilx <swarooppatil850@gmail.com>
2024-12-15 14:57:23 +0530

    comparison with std::cmp, match

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -10,6 +10,11 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
-    println!("You guessed {}", guess);
-    println!("The secret number is {secret_number}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number"); //shadow variable of type 32 bit unsigned integer
+    println!("Your guess: {}, secret number: {}", guess, secret_number);
+    match guess.cmp(&secret_number){
+     Ordering::Less => println!("Too small"),
+     Ordering::Greater => println!("Too big"),
+     Ordering::Equal => println!("Perfect, You win!"),
+    }
 }
