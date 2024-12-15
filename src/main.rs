f787345f84dd841b6e36647b3eb75fc1876df97c
swarooppatilx <swarooppatil850@gmail.com>
2024-12-15 14:34:54 +0530

    variables and mutablity

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess: ");
-    let mut guess = String::new();
-
+    let mut guess = String::new(); //mutable variable in rust
+    let apple = 69; //immutable variable or constant in other languages, by default variables are immutable in rust pretty wierd!
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
     println!("You guessed {}", guess);
+    println!("The value of apple {apple}");
 }
